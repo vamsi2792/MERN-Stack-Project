9a48fa1decc7973b58cde48fc216b455ff55ee2c
vamsi2792 <vamsi.17753@gmail.com>
2024-11-20 10:23:42 -0500

    Updated the URL

--- ecommerce/frontend/src/components/Constants.js
+++ ecommerce/frontend/src/components/Constants.js
@@ -1,4 +1,7 @@
 // export const BACKEND_API_URL ="http://35.175.219.15:5000/api"
 // export const BACKEND_API_URL ="http://localhost:5000/api"
 
-export const BACKEND_API_URL ="http://54.89.255.128:9000/api"
+// export const BACKEND_API_URL ="http://54.89.255.128:9000/api"
+
+
+export const BACKEND_API_URL ="http://98.82.167.164:3000/api"
